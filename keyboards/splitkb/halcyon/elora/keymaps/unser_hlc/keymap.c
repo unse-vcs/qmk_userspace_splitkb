9698acae498eaaec05e29cf1905694495da57a9d
unse-vcs <96228654+unse-vcs@users.noreply.github.com>
2026-08-22 00:54:32 +0200

    keymap: mirror macOS and Windows layer switching

--- keyboards/splitkb/halcyon/elora/keymaps/unser_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/unser_hlc/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Windows: symbols and shifted punctuation.
     [LAYER_WIN_SPEC] = LAYOUT(
         // First row: left six keys | right six keys
-        _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+        TO_MAC,  _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
         // Second row: left six keys | right six keys
         _______, DE_CIRC, DE_DEG,  DE_LABK, DE_RABK, DE_PIPE,                         DE_TILD, DE_LBRC, DE_RBRC, DE_LCBR, DE_RCBR, XXXXXXX,
         // Home row (third row): left six keys | right six keys
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // Fourth row: right six regular keys
         DE_N,    DE_M,    DE_COMM, DE_DOT, DE_MINS, KC_RCTL,
         // Fifth row: left five lower-thumb keys | right five lower-thumb keys
-                                      KC_LCMD, KC_LOPT, KC_BSPC, KC_SPC,  KC_ENT,  KC_ENT,  KC_SPC,  KC_BSPC, KC_ROPT, KC_RCMD
+                                       KC_LCMD, KC_LOPT, KC_BSPC, KC_SPC,  LT(LAYER_MAC_NAVNUM, KC_ENT),  TT(LAYER_MAC_NAVNUM),  TT(LAYER_MAC_SPEC),  KC_BSPC, KC_ROPT, KC_RCMD
     ),
 
     // macOS: navigation, editing, numpad, and media.
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // macOS: symbols and shifted punctuation.
     [LAYER_MAC_SPEC] = LAYOUT(
         // First row: left six keys | right six keys
-        _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
+        TO_WIN, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
         // Second row: left six keys | right six keys
         _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,                            KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         // Home row (third row): left six keys | right six keys
